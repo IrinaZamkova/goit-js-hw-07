3a891e725008a7dff4f936c77524ea6681703167
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-07-20 21:08:12 +0300

    Update module-4.js

--- js/module-4.js
+++ js/module-4.js
@@ -1,16 +1,34 @@
 
-const btnIncrRef = document.querySelector("[data-action=increment]");
-const btnDecRef = document.querySelector("[data-action=decrement]");
+// const btnIncrRef = document.querySelector("[data-action=increment]");
+// const btnDecRef = document.querySelector("[data-action=decrement]");
 
-let counterValue = 0;
+// let counterValue = 0;
+
+// const counterRef = document.querySelector(`#value`);
+
+// btnDecRef.addEventListener(`click`, () => {
+//   counterRef.textContent = counterValue -= 1;
+// });
+
+// btnIncrRef.addEventListener(`click`, () => {
+//   counterRef.textContent = counterValue += 1;
+// });
+
+// это вариант чтоб не использовать однообразную функцию посмотрите пожалуйста 
+
+ const refs = {
+  tags: document.querySelector("#counter"),
+};
+refs.tags.addEventListener(`click`, counterClick);
 
 const counterRef = document.querySelector(`#value`);
+let counterValue = 0;
+function counterClick(event) {
+  if (event.target.nodeName !== `BUTTON`) {
+    return;
+  }
 
-btnDecRef.addEventListener(`click`, () => {
-  counterRef.textContent = counterValue -= 1;
-});
-
-btnIncrRef.addEventListener(`click`, () => {
-  counterRef.textContent = counterValue += 1;
-});
-
+  event.target.dataset.action === "increment"
+    ? (counterRef.textContent = counterValue += 1)
+    : (counterRef.textContent = counterValue -= 1);
+}
