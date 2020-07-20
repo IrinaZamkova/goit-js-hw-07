37cd8e5a7d9813d06e03303376d330d7135d0e09
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-07-20 21:37:02 +0300

    Update module-4.js

--- js/module-4.js
+++ js/module-4.js
@@ -16,19 +16,32 @@
 
 // это вариант чтоб не использовать однообразную функцию посмотрите пожалуйста 
 
- const refs = {
-  tags: document.querySelector("#counter"),
-};
-refs.tags.addEventListener(`click`, counterClick);
+//  const refs = {
+//   tags: document.querySelector("#counter"),
+// };
+// refs.tags.addEventListener(`click`, counterClick);
 
+// const counterRef = document.querySelector(`#value`);
+// let counterValue = 0;
+// function counterClick(event) {
+//   if (event.target.nodeName !== `BUTTON`) {
+//     return;
+//   }
+
+//   event.target.dataset.action === "increment"
+//     ? (counterRef.textContent = counterValue += 1)
+//     : (counterRef.textContent = counterValue -= 1);
+// }
+// правильный!
+
+ const btnIncrRef = document.querySelector("[data-action=increment]");
+const btnDecRef = document.querySelector("[data-action=decrement]");
 const counterRef = document.querySelector(`#value`);
 let counterValue = 0;
-function counterClick(event) {
-  if (event.target.nodeName !== `BUTTON`) {
-    return;
-  }
 
-  event.target.dataset.action === "increment"
-    ? (counterRef.textContent = counterValue += 1)
-    : (counterRef.textContent = counterValue -= 1);
+btnDecRef.addEventListener(`click`,counterClick);
+btnIncrRef.addEventListener(`click`, counterClick);
+ 
+function counterClick({target: {name}}) {
+  counterRef.textContent = name === "increment" ? ++counterValue : --counterValue;
 }
